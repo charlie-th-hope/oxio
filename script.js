741b2e7d8ea633179b17692db5173fa447a13cea
Charlie TH Hope <118636873+charlie-th-hope@users.noreply.github.com>
2025-02-01 13:55:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function showSection(sectionId) {
+    document.getElementById("default-section").classList.add("hidden");
+    document.getElementById("qa").classList.add("hidden");
+    document.getElementById("moving-home").classList.add("hidden");
+
+    if (sectionId === 'default') {
+        document.getElementById("default-section").classList.remove("hidden");
+    } else {
+        document.getElementById(sectionId).classList.remove("hidden");
+    }
+
+    document.querySelectorAll(".nav-menu button").forEach(button => button.classList.remove("active"));
+
+    document.querySelector(`button[onclick="showSection('${sectionId}')"]`).classList.add("active");
+}
+
+document.getElementById("copyButton").addEventListener("click", function() {
+    navigator.clipboard.writeText("RY8TUW6");
+    alert("Referral code copied!");
+});
