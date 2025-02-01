026764975c029b950399f37ce2d1de5a880d05f8
Charlie TH Hope <118636873+charlie-th-hope@users.noreply.github.com>
2025-02-01 16:04:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,11 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
+ var xhr = new XMLHttpRequest();
+    xhr.open("GET", "qa.html", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.getElementById("qa-content").innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
